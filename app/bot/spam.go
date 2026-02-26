1a97f301b4f1d54531cb37653af6104527a0fcfe
brolin <alexander.brolin@polati.ru>
2026-02-26 14:07:30 +0300

    debug on

--- app/bot/spam.go
+++ app/bot/spam.go
@@ -162,7 +162,7 @@ func (s *SpamFilter) OnMessage(msg Message, checkOnly bool) (response Response) 
 			ChannelID: msg.SenderChat.ID,
 		}
 	}
-	log.Printf("[DEBUG] user %s is not a spammer, %s", displayUsername, checkResultStr)
+	log.Printf("[INFO] user %s is not a spammer, checks: %s, msg: %q", displayUsername, checkResultStr, msgText)
 	return Response{CheckResults: checkResults} // not a spam
 }
 
